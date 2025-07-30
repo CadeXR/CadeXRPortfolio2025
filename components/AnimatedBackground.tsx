e6dc3112c3181f31ee2f4963ea496a30aa61268f
CadeXR <54557425+CadeXR@users.noreply.github.com>
2025-07-30 18:51:55 -0400

    sigh

--- components/AnimatedBackground.tsx
+++ components/AnimatedBackground.tsx
@@ -104,7 +104,9 @@ export default function AnimatedBackground() {
   if (!isClient) {
     return (
       <div className="fixed inset-0 z-0 bg-gradient-to-br from-black via-purple-900/20 to-blue-900/20">
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.1"%3E%3Ccircle cx="30" cy="30" r="1"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-20" />
+        <div className="absolute inset-0 opacity-20" style={{
+          backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.1'%3E%3Ccircle cx='30' cy='30' r='1'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`
+        }} />
       </div>
     )
   }
